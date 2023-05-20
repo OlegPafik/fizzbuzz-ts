23f8cef9bae5b2d7192533ce587efce0e802f8b0
Oleg <oleg.pafik@eversens.com>
2023-05-20 10:44:26 +0200

    Refactor Buzz case, when multiple of 5 but not FizzBuzz

--- src/fizzbuzz.ts
+++ src/fizzbuzz.ts
@@ -4,7 +4,7 @@ export const fizzBuzz = (input: number) => {
     result = "FizzBuzz";
   } else if (input % 3 === 0) {
     result = "Fizz";
-  } else if (input === 5 || input === 10 || input === 20) {
+  } else if (input % 5 === 0) {
     result = "Buzz";
   } else {
     result = input.toString();
