40af661991940b786c229afefa3dce1d253367a3
Oleg <oleg.pafik@eversens.com>
2023-05-20 10:50:54 +0200

    Refactor removing if else for if with guard clause

--- src/fizzbuzz.ts
+++ src/fizzbuzz.ts
@@ -1,11 +1,12 @@
 export const fizzBuzz = (input: number) => {
   if (input % 3 === 0 && input % 5 === 0) {
     return "FizzBuzz";
-  } else if (input % 3 === 0) {
-    return "Fizz";
-  } else if (input % 5 === 0) {
-    return "Buzz";
-  } else {
-    return input.toString();
   }
+  if (input % 3 === 0) {
+    return "Fizz";
+  }
+  if (input % 5 === 0) {
+    return "Buzz";
+  }
+  return input.toString();
 };
