5cf6ac5ff1b425ecfc5e4ad8a86ca9c36e644b06
Oleg <oleg.pafik@eversens.com>
2023-05-20 11:04:50 +0200

    Add all FizzBuzz cases without new requirements

--- test/fizzbuzz.test.ts
+++ test/fizzbuzz.test.ts
@@ -4,43 +4,33 @@ describe("Non special numbers", () => {
   it("1 is 1", () => {
     expect(fizzBuzz(1)).toEqual("1");
   });
-
   it("2 is 2", () => {
     expect(fizzBuzz(2)).toEqual("2");
   });
-
   it("4 is 4", () => {
     expect(fizzBuzz(4)).toEqual("4");
   });
-
   it("7 is 7", () => {
     expect(fizzBuzz(7)).toEqual("7");
   });
-
   it("8 is 8", () => {
     expect(fizzBuzz(8)).toEqual("8");
   });
-
   it("11 is 11", () => {
     expect(fizzBuzz(11)).toEqual("11");
   });
-
   it("13 is 13", () => {
     expect(fizzBuzz(13)).toEqual("13");
-  });
-
+  })
   it("14 is 14", () => {
     expect(fizzBuzz(14)).toEqual("14");
   });
-
   it("16 is 16", () => {
     expect(fizzBuzz(16)).toEqual("16");
   });
-
   it("17 is 17", () => {
     expect(fizzBuzz(17)).toEqual("17");
   });
-
   it("19 is 19", () => {
     expect(fizzBuzz(19)).toEqual("19");
   });
@@ -56,11 +46,9 @@ describe("Fizz number", () => {
   it("9 is Fizz", () => {
     expect(fizzBuzz(9)).toEqual("Fizz");
   });
-
   it("12 is Fizz", () => {
     expect(fizzBuzz(12)).toEqual("Fizz");
   });
-
   it("18 is Fizz", () => {
     expect(fizzBuzz(18)).toEqual("Fizz");
   });
@@ -70,11 +58,9 @@ describe("Buzz number", () => {
   it("5 is Buzz", () => {
     expect(fizzBuzz(5)).toEqual("Buzz");
   });
-
   it("10 is Buzz", () => {
     expect(fizzBuzz(10)).toEqual("Buzz");
   });
-
   it("20 is Buzz", () => {
     expect(fizzBuzz(20)).toEqual("Buzz");
   });
@@ -86,5 +72,17 @@ describe("FizzBuzz number", () => {
   });
   it("30 is FizzBuzz", () => {
     expect(fizzBuzz(30)).toEqual("FizzBuzz");
+  });
+  it("45 is FizzBuzz", () => {
+    expect(fizzBuzz(45)).toEqual("FizzBuzz");
+  });
+  it("60 is FizzBuzz", () => {
+    expect(fizzBuzz(60)).toEqual("FizzBuzz");
+  });
+  it("75 is FizzBuzz", () => {
+    expect(fizzBuzz(75)).toEqual("FizzBuzz");
+  });
+  it("90 is FizzBuzz", () => {
+    expect(fizzBuzz(90)).toEqual("FizzBuzz");
   });
 });
