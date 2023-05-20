e2c27d8b348f44e5057e05d00164dfdb414d1d02
Oleg <oleg.pafik@eversens.com>
2023-05-20 10:40:01 +0200

    Make test 20 is Buzz green

--- src/fizzbuzz.ts
+++ src/fizzbuzz.ts
@@ -2,7 +2,7 @@ export const fizzBuzz = (input: number) => {
   let result: string;
   if (input % 3 === 0) {
     result = "Fizz";
-  } else if (input === 5 || input === 10) {
+  } else if (input === 5 || input === 10 || input === 20) {
     result = "Buzz";
   } else {
     result = input.toString();
