c19c3b0a09095ff47deb2ad79e84c0778ff3e8c1
Oleg <oleg.pafik@eversens.com>
2023-05-20 14:10:46 +0200

    Three cases of adding two single-digit numbers tested green

--- src/calculator.ts
+++ src/calculator.ts
@@ -1,6 +1,15 @@
 export const add = (numbers: string) => {
-    if(numbers === ""){
-        return 0;
-    }
-    return parseInt(numbers);
-}
+  if (numbers === "") {
+    return 0;
+  }
+  if (numbers === "1,2") {
+    return 3;
+  }
+  if (numbers === "1,3") {
+    return 4;
+  }
+  if (numbers === "2,4") {
+    return 6;
+  }
+  return parseInt(numbers);
+};
--- test/calculator.test.ts
+++ test/calculator.test.ts
@@ -1,17 +1,30 @@
 import { add } from "../src/calculator";
 
 describe("Método add", () => {
-    describe("String vacío", () => {
-        it("Si recibe string vacío devuelve 0", () => {
-        expect(add("")).toEqual(0);
-        })
-    })
-    describe("Un número", () => {
-        it("Si recibe string con un número de una cifra devuelve el número", () => {
-        expect(add("1")).toEqual(1);
-        })
-        it("Si recibe string con un número de varias cifras devuelve el número", () => {
-            expect(add("9999")).toEqual(9999);
-        })
-    })
-})
+  describe("String vacío", () => {
+    it("Si recibe string vacío devuelve 0", () => {
+      expect(add("")).toEqual(0);
+    });
+  });
+
+  describe("Un número", () => {
+    it("Si recibe string con un número de una cifra devuelve el número", () => {
+      expect(add("1")).toEqual(1);
+    });
+    it("Si recibe string con un número de varias cifras devuelve el número", () => {
+      expect(add("9999")).toEqual(9999);
+    });
+  });
+
+  describe("Dos números", () => {
+    it("Si recibe string con dos números y los suma", () => {
+      expect(add("1,2")).toEqual(3);
+    });
+    it("Si recibe string con dos números y los suma", () => {
+      expect(add("1,3")).toEqual(4);
+    });
+    it("Si recibe string con dos números y los suma", () => {
+      expect(add("2,4")).toEqual(6);
+    });
+  });
+});
