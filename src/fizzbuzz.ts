bb953e6f3003114b64fa5f9f009eb6f72b30099f
Oleg <oleg.pafik@eversens.com>
2023-05-20 10:15:15 +0200

    Make 6 is Fizz test green

--- src/fizzbuzz.ts
+++ src/fizzbuzz.ts
@@ -1,6 +1,6 @@
 export const fizzBuzz = (input: number) => {
   let result: string;
-  if (input === 3) {
+  if (input === 3 || input === 6) {
     result = "Fizz";
   } else {
     result = input.toString();
